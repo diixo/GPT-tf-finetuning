7287236c5d55762b619c6d9686a6b65d435f4f34
diixo <diixo.com@gmail.com>
2025-04-11 14:48:44 +0300

    noomo dictionary commit

--- tf_tokenize_noomo.py
+++ tf_tokenize_noomo.py
@@ -5,7 +5,7 @@ from tokenizers.decoders import ByteLevel as ByteLevelDecoder
 from tokenizers.normalizers import Sequence, Lowercase
 from tokenizers.pre_tokenizers import ByteLevel
 from tokenizers.trainers import BpeTrainer
-from transformers import PreTrainedTokenizerFast
+from transformers import PreTrainedTokenizerFast, GPT2TokenizerFast
 import tensorflow as tf
 import numpy as np
 
@@ -20,8 +20,8 @@ tokenizer.normalizer = Sequence([Lowercase()])
 tokenizer.pre_tokenizer = ByteLevel()
 tokenizer.decoder = ByteLevelDecoder()
 
-trainer = BpeTrainer(vocab_size=50000, initial_alphabet=ByteLevel.alphabet(), special_tokens=[
-    "<pad>", "<s>", "</s>", "<unk>", "<mask>"
+trainer = BpeTrainer(vocab_size=50000, initial_alphabet=ByteLevel.alphabet(), min_frequency=1,
+    special_tokens=["<pad>", "<s>", "</s>", "<unk>", "<mask>"
     ])
 
 tokenizer.train(["data/synthetic-data.txt"], trainer)
@@ -36,3 +36,23 @@ fast_tokenizer = PreTrainedTokenizerFast(
 )
 
 fast_tokenizer.save_pretrained(tokenizer_path)
+
+##########################################################################################
+
+tokenizer_gpt = GPT2TokenizerFast.from_pretrained(tokenizer_path)
+
+print(f"model.config: vocab.sz={tokenizer_gpt.vocab_size},",
+    f"pad_token_id={tokenizer_gpt.pad_token_id},",
+    f"bos_token_id={tokenizer_gpt.bos_token_id},",
+    f"eos_token_id={tokenizer_gpt.eos_token_id}",
+    )
+
+##########################################################################################
+
+input_ids = tokenizer_gpt("Learning is hearing", add_special_tokens=False, padding=False, return_tensors="np")
+input_ids = input_ids["input_ids"]
+input_ids = input_ids[0]
+
+#print(input_ids)
+print(tokenizer_gpt.convert_ids_to_tokens(input_ids))
+print(tokenizer_gpt.decode(input_ids, skip_special_tokens=False))
