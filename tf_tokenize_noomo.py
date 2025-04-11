57544bb2cd51c39383b77d9237d2afb1ba29d5f4
diixo <diixo.com@gmail.com>
2025-04-11 13:25:52 +0300

    tf_tokenize_noomo.py: initial commit

---
+++ tf_tokenize_noomo.py
@@ -0,0 +1,38 @@
+
+from tokenizers.models import BPE
+from tokenizers import Tokenizer
+from tokenizers.decoders import ByteLevel as ByteLevelDecoder
+from tokenizers.normalizers import Sequence, Lowercase
+from tokenizers.pre_tokenizers import ByteLevel
+from tokenizers.trainers import BpeTrainer
+from transformers import PreTrainedTokenizerFast
+import tensorflow as tf
+import numpy as np
+
+
+
+tokenizer_path  = "noomo"
+
+##########################################################################################
+
+tokenizer = Tokenizer(BPE())
+tokenizer.normalizer = Sequence([Lowercase()])
+tokenizer.pre_tokenizer = ByteLevel()
+tokenizer.decoder = ByteLevelDecoder()
+
+trainer = BpeTrainer(vocab_size=50000, initial_alphabet=ByteLevel.alphabet(), special_tokens=[
+    "<pad>", "<s>", "</s>", "<unk>", "<mask>"
+    ])
+
+tokenizer.train([], trainer)
+
+fast_tokenizer = PreTrainedTokenizerFast(
+    tokenizer_object = tokenizer,
+    bos_token = "<s>",
+    eos_token = "</s>",
+    unk_token = "<unk>",
+    pad_token = "<pad>",
+    mask_token = "<mask>"
+)
+
+fast_tokenizer.save_pretrained(tokenizer_path)
