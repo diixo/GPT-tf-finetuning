8d2656bc4ff44414765cb3a9e6042510db300050
diixo <diixo.com@gmail.com>
2025-04-15 15:19:14 +0300

    tf_dictionary.py: ++ opus-mt-en-ru

--- tf_dictionary.py
+++ tf_dictionary.py
@@ -33,3 +33,23 @@ print(len(difference))
 
 with open("data/difference.json", "w", encoding="utf-8") as f:
     json.dump(sorted(difference), f, ensure_ascii=False, indent=4)
+
+
+#######################################################################
+
+from transformers import MarianMTModel, MarianTokenizer
+
+tokenizer = MarianTokenizer.from_pretrained("./opus-mt-en-ru")
+model = MarianMTModel.from_pretrained("./opus-mt-en-ru")
+
+texts = ["This is a test.", "Is it a text of news?", "It is a text of news.", "Where is a text of news?"]
+inputs = tokenizer(texts, return_tensors="pt", padding=True)
+translated = model.generate(**inputs)
+
+translated_texts = [tokenizer.decode(t, skip_special_tokens=True) for t in translated]
+
+for original, translated in zip(texts, translated_texts):
+    print(f"{original} → {translated}")
+
+#print(translated_texts)
+
